e92e4d7898b95c19871251a5f0aa290732bde835
jjurm <micko.juraj64@gmail.com>
2020-01-19 23:12:58 +0000

    metric subcommand

---
+++ src/main/java/uk/ac/cam/jm2186/partii/cli/MetricSubcommand.kt
@@ -0,0 +1,25 @@
+package uk.ac.cam.jm2186.partii.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import uk.ac.cam.jm2186.partii.metric.MetricType
+
+class MetricSubcommand : NoRunCliktCommand(
+    name = "metric",
+    printHelpOnEmptyArgs = true
+) {
+
+    init {
+        subcommands(ListMetricsCommand())
+    }
+
+    class ListMetricsCommand : CliktCommand(name = "list", help = "List available graph metrics") {
+        override fun run() {
+            MetricType.values().forEach {
+                println("${it.id} : ${it.resultClass.name}")
+            }
+        }
+    }
+
+}
---
+++ src/main/java/uk/ac/cam/jm2186/partii/metric/MetricType.kt
@@ -0,0 +1,23 @@
+package uk.ac.cam.jm2186.partii.metric
+
+import java.io.Serializable
+
+class MetricType<Result : Serializable> private constructor(
+    val id: String,
+    val metricFactory: MetricFactory<Result>,
+    val resultClass: Class<Result>
+) {
+    companion object {
+        private val metrics = mutableMapOf<String, MetricType<*>>()
+
+        private fun register(metricType: MetricType<*>) {
+            metrics[metricType.id] = metricType
+        }
+
+        fun values(): Collection<MetricType<*>> = metrics.values
+
+        init {
+            register(MetricType("AverageDegree", AverageDegreeMetric.Factory(), Double::class.java))
+        }
+    }
+}
