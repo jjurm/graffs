c158545309144988e51a968c2b2116bb8fca6317
jjurm <micko.juraj64@gmail.com>
2020-01-22 23:28:36 +0000

    betweenness centrality metrics

--- src/main/java/uk/ac/cam/jm2186/partii/metric/MetricType.kt
+++ src/main/java/uk/ac/cam/jm2186/partii/metric/MetricType.kt
@@ -4,23 +4,34 @@ import java.io.Serializable
 
 typealias MetricId = String
 
-class MetricType<Result : Serializable> private constructor(
+interface MetricFactory : Serializable {
+    fun createMetric(params: List<Number>): Metric
+}
+
+class MetricType private constructor(
     val id: MetricId,
-    val metricFactory: MetricFactory<Result>,
-    val resultClass: Class<Result>
+    val metricFactory: MetricFactory
 ) : Serializable {
     companion object {
-        private val metrics = mutableMapOf<MetricId, MetricType<*>>()
+        private val metrics = mutableMapOf<MetricId, MetricType>()
 
-        private fun register(metricType: MetricType<*>) {
+        private fun register(
+            id: MetricId,
+            metricFactory: MetricFactory
+        ) {
+            val metricType = MetricType(id, metricFactory)
             metrics[metricType.id] = metricType
         }
 
-        fun values(): Collection<MetricType<*>> = metrics.values
+        fun values(): Collection<MetricType> = metrics.values
         fun ids(): Collection<MetricId> = metrics.keys
 
+        fun byId(id: MetricId): MetricType = metrics.getValue(id)
+
         init {
-            register(MetricType("AverageDegree", AverageDegreeMetric.Factory(), Double::class.java))
+            register("AverageDegree", AverageDegreeMetric.Factory())
+            register("BetweennessCentrality", BetweennessCentralityMetric.Factory())
+            register("AverageBetweennessCentrality", AverageBetweennessCentralityMetric.Factory())
         }
     }
 }
