7bb83d0dcb55972db98a4bff21f15d20e669e6d9
jjurm <micko.juraj64@gmail.com>
2020-01-22 11:03:39 +0000

    Specify primary keys in MetricExperiment

---
+++ src/main/java/uk/ac/cam/jm2186/partii/storage/IdClassEntity.kt
@@ -0,0 +1,3 @@
+package uk.ac.cam.jm2186.partii.storage
+
+annotation class IdClassEntity
--- src/main/java/uk/ac/cam/jm2186/partii/storage/model/MetricExperiment.kt
+++ src/main/java/uk/ac/cam/jm2186/partii/storage/model/MetricExperiment.kt
@@ -1,15 +1,24 @@
 package uk.ac.cam.jm2186.partii.storage.model
 
 import uk.ac.cam.jm2186.partii.metric.MetricId
-import uk.ac.cam.jm2186.partii.storage.AbstractJpaPersistable
-import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.ManyToOne
+import uk.ac.cam.jm2186.partii.storage.IdClassEntity
+import java.io.Serializable
+import javax.persistence.*
 
 @Entity
+@IdClass(MetricExperimentId::class)
 class MetricExperiment(
-    val metricId: MetricId,
-    @ManyToOne(fetch = FetchType.EAGER)
+    @Id val metricId: MetricId,
+    @Id @ManyToOne(fetch = FetchType.EAGER)
     val graph: GeneratedGraph,
     val value: Double
-) : AbstractJpaPersistable<Long>()
+) : Serializable
+
+@IdClassEntity
+class MetricExperimentId(
+    val metricId: MetricId,
+    val graph: GeneratedGraph
+) : Serializable {
+    operator fun component1() = metricId
+    operator fun component2() = graph
+}
