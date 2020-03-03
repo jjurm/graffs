f8c4d248ec610c46f18d8392badf6cdc33518976
jjurm <micko.juraj64@gmail.com>
2020-03-03 04:26:36 +0000

    Remove obsolete entities

--- src/main/java/uk/ac/cam/jm2186/graffs/cli/DatabaseSubcommand.kt
+++ src/main/java/uk/ac/cam/jm2186/graffs/cli/DatabaseSubcommand.kt
@@ -2,9 +2,6 @@ package uk.ac.cam.jm2186.graffs.cli
 
 import com.github.ajalt.clikt.core.NoRunCliktCommand
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import uk.ac.cam.jm2186.graffs.storage.model.Tag
 
 
 class DatabaseSubcommand : NoRunCliktCommand(
@@ -15,7 +12,6 @@ class DatabaseSubcommand : NoRunCliktCommand(
     init {
         subcommands(
             TestCommand(),
-            TruncateCommand(),
             ResetCommand()
         )
     }
@@ -24,36 +20,6 @@ class DatabaseSubcommand : NoRunCliktCommand(
         override fun run0() {
             if (hibernate.isConnected) println("Successfully connected.")
             else System.err.println("Unsuccessful (see logs above)")
-        }
-    }
-
-    inner class TruncateCommand : AbstractHibernateCommand(name = "truncate", help = "Truncate parts of the databse") {
-
-        private val tag by option(
-            "--tag",
-            "--tags",
-            help = "Delete all database entries marked with the specified tag(s), delimited by comma"
-        ).required()
-
-        override fun run0() {
-            hibernate.beginTransaction()
-
-            val tag = hibernate.get(Tag::class.java, tag)
-            hibernate.delete(tag)
-
-            /*val builder = hibernate.criteriaBuilder
-
-            val query = builder.createQuery(DistortedGraph::class.java)
-            val root = query.from(DistortedGraph::class.java)
-            query.where(
-                builder.equal(root.get<String>(DistortedGraph_.tag), tag)
-            )
-            val graphs = hibernate.createQuery(query).list()
-
-            graphs.forEach { hibernate.delete(it) }*/
-
-            hibernate.transaction.commit()
-            //println("Deleted ${graphs.size} generated graphs, and related experiments")
         }
     }
 
--- src/main/java/uk/ac/cam/jm2186/graffs/storage/model/package-info.kt
+++ src/main/java/uk/ac/cam/jm2186/graffs/storage/model/package-info.kt
@@ -2,10 +2,6 @@ package uk.ac.cam.jm2186.graffs.storage.model
 
 // this is so that the models can easily be loaded
 internal val entities = listOf(
-    DistortedGraphOld::class,
-    MetricExperiment::class,
-    Tag::class,
-
     Experiment::class,
     GraphGenerator::class,
     GraphCollection::class,
