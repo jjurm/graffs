0be716e9b856225a2f883d90731f1f72dbf35b26
jjurm <micko.juraj64@gmail.com>
2020-03-18 15:40:11 +0400

    Fix tests

--- src/test/java/uk/ac/cam/jm2186/graffs/graph/RemovingEdgesGraphProducerTest.kt
+++ src/test/java/uk/ac/cam/jm2186/graffs/graph/RemovingEdgesGraphProducerTest.kt
@@ -6,55 +6,27 @@ import uk.ac.cam.jm2186.graffs.GraphAssert
 internal class RemovingEdgesGraphProducerTest {
 
     @Test
-    fun produceAndCompute() {
+    fun produce() {
         val graph = GraphTestUtils.generateSmallGraph()
         val producer = RemovingEdgesGenerator(
-            graph,
             deletionRate = 0.3,
             seed = 42
         )
-        val generated = producer.produce()
-        producer.compute()
+        val generated = producer.produce(graph, 1)[0]
 
         GraphAssert.assertGraphEquals(
             """
             0
-            1
+            1 [1->0_1]
             2
-            3 [3->1_3]
-            4 [4->2_4]
-            5
-            6 [6->2_6]
+            3
+            4
+            5 [5->4_5,5->0_5]
+            6
             7 [7->5_7,7->2_7,7->3_7,7->4_7]
             8 [8->0_8]
-            9 [9->0_9,9->3_9,9->5_9,9->6_9]""",
-            generated
-        )
-    }
-
-    @Test
-    fun produceComputeCombined() {
-        val graph = GraphTestUtils.generateSmallGraph()
-        val producer = RemovingEdgesGenerator(
-            graph,
-            deletionRate = 0.3,
-            seed = 42
-        )
-        val generated = producer.produceComputed()
-
-        GraphAssert.assertGraphEquals(
-            """
-            0
-            1
-            2
-            3 [3->1_3]
-            4 [4->3_4,4->2_4]
-            5 [5->0_5,5->3_5]
-            6
-            7 [7->4_7,7->2_7]
-            8 [8->0_8]
-            9 [9->0_9,9->8_9,9->5_9,9->6_9]""",
-            generated
+            9 [9->0_9,9->3_9,9->6_9]""",
+            generated.graph
         )
     }
 }
